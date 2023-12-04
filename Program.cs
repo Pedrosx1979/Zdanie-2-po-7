573fe6e93dd51a8baa9137794cf6f58adde57b77
Piotr Szydlak <pedrosx@o2.pl>
2023-12-04 09:39:17 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Wprowadź liczbę  : "); //  deklaracja liczby dp zbadnia
+int a = Int32.Parse(Console.ReadLine()); // odczytanie liczby dp zbadnia
+
+
+
+if (a % 2 == 0)  //sprawdanie warunku parzystości liczby
+{
+    Console.WriteLine($"Liczba {a} jest parzysta");
+    Console.ReadLine();
+}
+else
+{
+    Console.WriteLine($"Liczba {a}  nie jest parzysta");
+    Console.ReadLine();
+};
